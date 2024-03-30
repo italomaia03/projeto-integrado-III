df6ef8be372efa2531f37e060d66d560190bff4f
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-30 14:54:46 -0300

    feat: adicionar chamada do modal

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,9 @@ import { ProdutoService } from "./service/produto.service.js";
 import { Tabela } from "./view/elements/tabela/tabela.view.js";
 import { produtos } from "./db/dados.db.js"; 
 import { AcoesTabela } from "./view/elements/tabela/acoesTabela.view.js";
+import { Modal } from "./view/elements/modal/modal.view.js";
 
+const main = document.getElementById("produtos");
 const cadastrarBtn = document.getElementById("new-product");
 const avaliarBtn = document.getElementById("view-product");
 const atualizarBtn = document.getElementById("update-product");
@@ -16,6 +18,44 @@ const acoesProdutos = [
   {nome: "Remover Produto", src: "../assets/icons/delete.svg"}
 ]
 
+const propsModalProduto = {
+  tituloModal: "Cadastrar Produto",
+  camposForm: [
+    {
+      nome: "Nome do Produto",
+      tipo: "text",
+      id: "nomeProduto",
+      classe: "product-field"
+    },
+    {
+      nome: "Foto",
+      tipo: "file",
+      id: "fotoProduto"
+    },
+    {
+      nome: "Classificação do Produto",
+      tipo: "text",
+      id: "classificacaoProduto",
+      classe: "product-field"
+    },
+    {
+      nome: "Marca do Produto",
+      tipo: "text",
+      id: "marcaProduto",
+      classe: "product-field"
+    },
+    {
+      nome: "Preço do Produto",
+      tipo: "number",
+      id: "precoProduto",
+      classe: "product-field"
+    }
+  ]
+};
+
+const modalCadastro = new Modal(main, propsModalProduto);
+const telaCadastro = modalCadastro.executar();
+
 const acoesTabelaProdutos = new AcoesTabela(acoesProdutos).executar(); 
 
 const tabelaProdutos = new Tabela(
@@ -28,5 +68,11 @@ const tabelaProdutos = new Tabela(
 tabelaProdutos.render();
 
 cadastrarBtn.addEventListener("click", () => {
-  console.log("working...");
+  telaCadastro.showModal();
 });
+
+const fecharModalBtn = document.querySelector(".close-modal");
+
+fecharModalBtn.addEventListener("click", () => {
+  telaCadastro.close();
+})
