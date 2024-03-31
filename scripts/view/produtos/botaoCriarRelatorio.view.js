92f0903e5c1f08221db9fd0ef4752d7133eb6d60
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-31 13:28:25 -0300

    feat: criar componente de botão para gerar relatórios

---
+++ scripts/view/produtos/botaoCriarRelatorio.view.js
@@ -0,0 +1,33 @@
+import { Botao } from "../elements/botao.view.js";
+
+export class BotaoCriarRelatorio {
+  constructor(props) {
+    this.props = props;
+  }
+
+  executar() {
+    const botao = new Botao({
+      tag: "button",
+      classe: "action",
+      id: "new-report",
+      acao: this.props.acao,
+    }).executar();
+
+    const scan = document.createElement("scan");
+    const conteinerImg = document.createElement("div");
+    const img = document.createElement("img");
+
+    scan.innerText = "Gerar Relatório";
+    
+    img.src = "../assets/icons/Parking Ticket.png";
+    img.alt = "Relatório";
+
+    conteinerImg.classList.add("icon-container");
+    conteinerImg.appendChild(img);
+    
+    botao.appendChild(conteinerImg);
+    botao.appendChild(scan);
+
+    return botao;
+  }
+}
