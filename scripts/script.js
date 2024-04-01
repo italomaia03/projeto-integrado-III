164bb6f781b761a107cc274f1def23548ce1851c
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-31 21:15:26 -0300

    feat: adaptar o código às mudanças das demais classes

--- scripts/script.js
+++ scripts/script.js
@@ -2,65 +2,29 @@ import { ProdutoService } from "./service/produto.service.js";
 import { produtos } from "./db/dados.db.js"; 
 import { Modal } from "./view/elements/modal/modal.view.js";
 import { ProdutosView } from "./view/produtos/produtosView.view.js";
+import { propsModalProduto } from "./view/elements/modal/modalCadastroProps.view.js";
 
 const main = document.querySelector("main");
-const div = document.querySelector(".container");
 const iframe = document.querySelector(".painel");
 const produtoBtn = document.querySelector("#products-button");
 const acessoRapidoBtn = document.querySelector("#quick-access-btn");
-const cadastrarBtn = document.getElementById("new-product");
-const avaliarBtn = document.getElementById("view-product");
-const atualizarBtn = document.getElementById("update-product");
-const deletarBtn = document.getElementById("delete-product");
-const listaProdutosConteiner = document.getElementById("products-list");
+
 
 const produtoService = new ProdutoService(produtos);
 
-const propsModalProduto = {
-  tituloModal: "Cadastrar Produto",
-  camposForm: [
-    {
-      nome: "Nome do Produto",
-      tipo: "text",
-      id: "nomeProduto",
-      classe: "product-field"
-    },
-    {
-      nome: "Foto",
-      tipo: "file",
-      id: "fotoProduto"
-    },
-    {
-      nome: "Classificação do Produto",
-      tipo: "text",
-      id: "classificacaoProduto",
-      classe: "product-field"
-    },
-    {
-      nome: "Marca do Produto",
-      tipo: "text",
-      id: "marcaProduto",
-      classe: "product-field"
-    },
-    {
-      nome: "Preço do Produto",
-      tipo: "number",
-      id: "precoProduto",
-      classe: "product-field"
-    }
-  ],
-  acao: (event) => {
-    event.preventDefault();
+main.removeChild(iframe)
+main.appendChild(iframe)
+
+const modalCadastro = new Modal(propsModalProduto).executar();
+
+Object.defineProperty(propsModalProduto, "acao", {
+  value: () => {
     produtoService.cadastrar("Produto");
-  }
-};
+  },
+  configurable: true
+});
 
-const modalCadastro = new Modal(main, propsModalProduto);
-const telaCadastro = modalCadastro.executar();
-
-const produtosView = new ProdutosView(produtoService, {
-  modal: telaCadastro
-}).executar();
+const produtosView = new ProdutosView(produtoService, modalCadastro).executar();
 
 acessoRapidoBtn.addEventListener("click", () => {
   if(main.hasChildNodes()){
