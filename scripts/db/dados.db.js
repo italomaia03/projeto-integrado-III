60893975b7d4e32adb5a40d6619e68595bf222dc
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-31 21:10:48 -0300

    feat: adicionar mais produtos para testar rolagem na tabela

--- scripts/db/dados.db.js
+++ scripts/db/dados.db.js
@@ -1,5 +1,12 @@
 import { Produto } from "../model/produto.model.js";
 
 export const produtos = [
-    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99)
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
+    new Produto("0000001", "Vinha Padre Cícero", "../assets/images/vinho_padre_cicero.png", "Bebida Alcoólica", "Vinhos LTDA", 4.99),
 ];
