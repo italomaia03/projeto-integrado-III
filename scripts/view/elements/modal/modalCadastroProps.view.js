e517e3bceef0026727c34a274eba75142517a17d
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-03-31 21:12:04 -0300

    feat: definir props do modal em uma variável a parte

---
+++ scripts/view/elements/modal/modalCadastroProps.view.js
@@ -0,0 +1,34 @@
+export const propsModalProduto = {
+    tituloModal: "Cadastrar Produto",
+    camposForm: [
+      {
+        nome: "Nome do Produto",
+        tipo: "text",
+        id: "nomeProduto",
+        classe: "product-field"
+      },
+      {
+        nome: "Foto",
+        tipo: "file",
+        id: "fotoProduto"
+      },
+      {
+        nome: "Classificação do Produto",
+        tipo: "text",
+        id: "classificacaoProduto",
+        classe: "product-field"
+      },
+      {
+        nome: "Marca do Produto",
+        tipo: "text",
+        id: "marcaProduto",
+        classe: "product-field"
+      },
+      {
+        nome: "Preço do Produto",
+        tipo: "number",
+        id: "precoProduto",
+        classe: "product-field"
+      }
+    ],
+  };
