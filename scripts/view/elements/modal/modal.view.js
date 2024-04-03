3fafa658002ff8da307253d0b01157f4589aa0f7
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-03 15:09:21 -0300

    refactor: receber fomulários a partir dos props

--- scripts/view/elements/modal/modal.view.js
+++ scripts/view/elements/modal/modal.view.js
@@ -1,44 +1,12 @@
-import { Botao } from "../botao.view.js";
-import { ItemFormulario } from "./itemFormulario.view.js";
+import { Botao } from "../botao/botao.view.js";
 
 export class Modal {
     constructor(props) {
         this.props = props;
     }
 
-    mapearCampos() {
-        const { camposForm } = this.props;
-        return camposForm.map(campo => new ItemFormulario(campo).executar());
-    }
-
-    criarFormulario() {
-        const form = document.createElement("form");
-        const botaoSalvarProps = {
-            tag: "input",
-            nome: "Salvar",
-            tipo: "submit",
-            classe: "modal-button",
-            tipoEvento: "click",
-            acao: (event) => {
-                event.preventDefault();
-                this.props.acao
-            }
-        };
-        const botaoSalvar = new Botao(botaoSalvarProps).executar();
-        form.classList.add("modal-form");
-        
-        const campos = this.mapearCampos();
-
-        campos.forEach(campo => form.appendChild(campo));
-
-        form.appendChild(botaoSalvar);
-        form.method = "POST";
-
-        return form;
-    }
-
     criarTitulo() {
-        const { tituloModal } = this.props;
+        const { tituloModal, id } = this.props;
         const tituloConteiner = document.createElement("div");
         const titulo = document.createElement("h2");
         const botaoProps = {
@@ -47,7 +15,7 @@ export class Modal {
             tipo: "button",
             classe: "close-modal",
             tipoEvento: "click",
-            acao: () => document.querySelector("dialog").close()
+            acao: () => document.getElementById(`${id}`).close()
         };
         const fecharModalBtn = new Botao(botaoProps).executar();
 
@@ -61,15 +29,16 @@ export class Modal {
     }
 
     executar() {
+        const { id, elementos } = this.props;
         const modal = document.createElement("dialog");
         const tituloModal = this.criarTitulo();
-        const formModal = this.criarFormulario();
-
         modal.appendChild(tituloModal);
-        modal.appendChild(formModal);
-
-        modal.classList.add("modal");
-
+        if(elementos){
+            elementos.forEach(elemento => {
+                modal.appendChild(elemento)});
+        }
+        modal.classList.add("modal")
+        modal.id = id;
         return modal;
     }
 }
