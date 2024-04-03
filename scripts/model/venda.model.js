3e9869dd3b9e8d2fbb2e7236368fb1ffa71b92bb
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-03 15:03:29 -0300

    feat: implementar classe model para vendas

---
+++ scripts/model/venda.model.js
@@ -0,0 +1,10 @@
+export class Venda {
+    constructor(idVenda, caixaVenda, dataVenda, nomeVendedor, totalVenda, situacaoVenda){
+        this.idVenda = idVenda;
+        this.caixaVenda = caixaVenda;
+        this.dataVenda = dataVenda;
+        this.nomeVendedor = nomeVendedor;
+        this.totalVenda = totalVenda;
+        this.situacaoVenda = situacaoVenda;
+    }
+}
