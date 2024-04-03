18e150e8c49fe3ecaf173e252a4d989ba53a4318
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-03 17:41:59 -0300

    feat: organizar props de acessoRapidoView em um único arquivo

---
+++ scripts/view/acessoRapido/acessoRapidoProps.view.js
@@ -0,0 +1,18 @@
+export const acessoRapidoProps = {
+    botoes: [
+        {
+            src: "assets/images/painel_compras.png"
+        },
+        {
+            src: "assets/images/painel_estoque.png"
+        },
+        {
+            classe: "products-btn",
+            src: "assets/images/painel_produtos.png"
+        },
+        {
+            classe: "sales-btn",
+            src: "assets/images/painel_vendas.png"
+        }
+    ]
+}
