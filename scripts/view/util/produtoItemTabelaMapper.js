1b0a480a23a2f332f6b926ed38d44c5d106a66c3
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-03 15:16:25 -0300

    feat: mudar retorno para array de colunas

--- scripts/view/util/produtoItemTabelaMapper.js
+++ scripts/view/util/produtoItemTabelaMapper.js
@@ -1,42 +1,51 @@
 import { AcoesTabela } from "../elements/tabela/acoesTabela.view.js";
+import { ItemTabela } from "../elements/tabela/itemTabela.view.js";
 
-export default function (produto, acoes) {
-    let {fotoProduto, precoProduto, ...atributos} = produto;
+export function produtoItemTabelaMapper(produto, acoes) {
+  let { fotoProduto, precoProduto, ...atributos } = produto;
 
-    precoProduto = `R$${precoProduto}`.replace(/\./,",");
-    
-    atributos = Object.values(atributos);
-    atributos.push(precoProduto);
-    
-    const colunas = atributos.map(atributo => {
-            const coluna = document.createElement("td");
-            coluna.innerText = atributo;
-            return coluna;
-        });
-    
-    const fotoColuna = document.createElement("td");
-    const acoesProduto = criarColunaAcoes(new AcoesTabela(acoes).executar());
-    const imgEl = criarColunaImagem({
-        fotoProduto, 
-        nomeProduto: produto.nomeProduto
-    });
-    fotoColuna.appendChild(imgEl)
-    colunas.splice(2,0,fotoColuna);
-    colunas.push(acoesProduto);
-    return colunas;
+  precoProduto = `R$${precoProduto}`.replace(/\./, ",");
+
+  atributos = Object.values(atributos);
+  atributos.push(precoProduto);
+
+  const colunas = atributos.map((atributo) => {
+    const coluna = document.createElement("td");
+    coluna.innerText = atributo;
+    return coluna;
+  });
+
+  const fotoColuna = document.createElement("td");
+  const acoesProduto = criarColunaAcoes(new AcoesTabela(acoes).executar());
+  const imgEl = criarColunaImagem({
+    fotoProduto,
+    nomeProduto: produto.nomeProduto,
+  });
+  fotoColuna.appendChild(imgEl);
+  colunas.splice(2, 0, fotoColuna);
+  colunas.push(acoesProduto);
+  return colunas;
 }
 function criarColunaImagem(imgEl) {
-    const conteiner = document.createElement("img");
-    const { fotoProduto, nomeProduto } = imgEl;
+  const conteiner = document.createElement("img");
+  const { fotoProduto, nomeProduto } = imgEl;
 
-    conteiner.src = fotoProduto;
-    conteiner.alt = nomeProduto;
-    
-    return conteiner;
+  conteiner.src = fotoProduto;
+  conteiner.alt = nomeProduto;
+  conteiner.classList.add("table-row-icon");
+
+  return conteiner;
 }
 
-function criarColunaAcoes(acoes) {
-    const acoesProduto = document.createElement("td");
-    acoesProduto.appendChild(acoes);
-    return acoesProduto;
+export function criarColunaAcoes(acoes) {
+  const acoesProduto = document.createElement("td");
+  acoesProduto.appendChild(acoes);
+  return acoesProduto;
+}
+
+export default function executarProdutoMapper(produtos, acoes) {
+  const linhas = produtos
+    .map((produto) => produtoItemTabelaMapper(produto, acoes))
+    .map((produto) => new ItemTabela(produto).executar());
+  return linhas;
 }
