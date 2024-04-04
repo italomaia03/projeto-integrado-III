8dd15f556cbb7063eb5d21b2afdd51bd8c227dda
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-04-03 21:26:35 -0300

    feat: executar a renderização apenas quando inserir na main

--- scripts/events/mudancaPagina.event.js
+++ scripts/events/mudancaPagina.event.js
@@ -1,6 +1,6 @@
 export default function (main, pagina) {
     if (main.hasChildNodes()) {
         main.lastChild.remove();
-        main.appendChild(pagina);
+        main.appendChild(pagina.executar());
       }
 }
